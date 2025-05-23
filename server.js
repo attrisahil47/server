8567c6024ed384330172abaf8f6268c751611df8
attrisahil47 <attrisahil47@gmail.com>
2025-05-23 09:22:57 +0530

    Add Frontend Url in origin in cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require("./Routes")
 backend.use(express.json());
  
 backend.use(cors({
-    origin: "*",
+    origin: ["https://frontend-one-gold-26.vercel.app"],
     methods: ["GET", "PUT", "PATCH", "POST", "DELETE"]
 
 })
